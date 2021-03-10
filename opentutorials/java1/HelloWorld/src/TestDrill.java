4311849b8f0857f9d1b99d7c313895be2f09fb6c
cjh <jihye0612@kesti.co.kr>
2021-03-10 18:39:00 +0900

    자바1 끝까지

---
+++ opentutorials/java1/HelloWorld/src/TestDrill.java
@@ -0,0 +1,14 @@
+public class TestDrill{
+
+    public static void main(String[] args){
+
+        String s = "\\    /\\\n"
+                + " )  ( ')\n"
+                + "(  /  )\n"
+                + " \\(__)|";
+
+        System.out.println(s);
+
+    }
+
+}
