1b159af7591798ccf9d8f225fa279846370a70d9
cjh <jihye0612@kesti.co.kr>
2021-03-19 18:33:01 +0900

    c 이중반복문까지

--- inflearn/c-elementary-to-game/NadoCoding/NadoCoding/loop.c
+++ inflearn/c-elementary-to-game/NadoCoding/NadoCoding/loop.c
@@ -34,10 +34,46 @@ int main(void)
 	// for, while, do while
 
 	// for(선언;조건;증감)
-	for (int i = 1; i <= 10; i++)
+	/*for (int i = 1; i <= 10; i++)
 	{
 		printf("Hello World! %d\n", i);
+	}*/
+
+	// while (조건) {  }
+	/*int i = 1;
+	while (i <= 10)
+	{
+		printf("Hello World! %d\n", i++);
+	}*/
+
+	// do {  } while (조건); 
+	/*int i = 1;
+	do {
+		printf("Hello World! %d\n", i++);
+	} while (i <= 10);*/
+
+	// 2중 반복문
+	/*for (int i = 1; i <= 3; i++) {
+
+		printf("첫 번째 반복문 : %d\n", i);
+
+		for (int j = 1; j <= 5; j++) {
+
+			printf("	두 번째 반복문 : %d\n", j);
+
+		}
+
+	}*/
+
+	// 구구단
+	for (int i = 2; i <= 9; i++) {
+		printf("%d단 계산\n", i);
+		for (int j = 1; j <=9; j++){
+			printf("  %d * %d = %d\n", i, j, i * j);
+		}
 	}
+
+
 
 
 	return 0;
